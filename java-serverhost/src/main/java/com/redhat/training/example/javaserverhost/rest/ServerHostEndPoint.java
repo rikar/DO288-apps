9a4066d94dbc22078a98bf9b4a44636b2e115679
Student User <student@workstation.lab.example.com>
2020-12-10 13:14:24 -0500

    Update1

--- java-serverhost/src/main/java/com/redhat/training/example/javaserverhost/rest/ServerHostEndPoint.java
+++ java-serverhost/src/main/java/com/redhat/training/example/javaserverhost/rest/ServerHostEndPoint.java
@@ -19,7 +19,7 @@ public class ServerHostEndPoint {
     catch (Exception e) {
        e.printStackTrace();
     }
-    String msg = "I am running on server "+host+" Version 1.0 \n";
+    String msg = "I am running on server "+host+" Version Ricaldao 2.10 \n";
     return Response.ok(msg).build();
   }
 }
